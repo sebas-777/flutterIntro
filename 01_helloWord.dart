bb0a790cbeef0534194e96582ec21bf092f8da8a
Sebastian Castro <scastro@qvision.com.co>
2024-09-10 06:35:09 -0500

    [sc] first commit

---
+++ 01_helloWord.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  // Dart is strongly typed language
+  // var myName = 'Dart';
+  //late final name;
+  // String name = 'Dart'; 
+  const name = 'Dart';
+  print('Hello, $name!');
+  print('Hello, ${name.toUpperCase()}');
+  print('Hello ${1 + 1}');
+}
