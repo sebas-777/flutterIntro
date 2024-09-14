1c30f7e9a9ac59addba1ee3dfaf5fb46f2c37d44
Sebastian Castro <scastro@qvision.com.co>
2024-09-13 20:52:51 -0500

    First commit

---
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '10',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text('Clicks',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w100)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
